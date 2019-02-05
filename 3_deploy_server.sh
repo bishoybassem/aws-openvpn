efc5015ff2ab64571ae3ec2f1aa20bb870e98933
bishoybassem <bishoybassem@gmail.com>
2019-02-05 09:27:49 +0100

    Added cfn-hup to handle stack updates with init changes

--- 3_deploy_server.sh
+++ 3_deploy_server.sh
@@ -7,13 +7,21 @@ if [[ "$KEY_PAIR_NAME" == "" ]]; then
     exit 1
 fi
 
-# Create the cloudformation stack from template file.
-aws cloudformation create-stack --stack-name openvpn --template-body file://cf_template.yml --parameters \
+ACTION=create-stack
+EVENT=stack-create-complete
+# If the stack already exists, then use the update action.
+if [[ $(aws cloudformation describe-stacks --stack-name openvpn 2> /dev/null) ]]; then
+    ACTION=update-stack
+    EVENT=stack-update-complete
+fi
+
+# Create/update the cloudformation stack from template file.
+aws cloudformation ${ACTION} --stack-name openvpn --template-body file://cf_template.yml --parameters \
     "ParameterKey=KeyName,ParameterValue=$KEY_PAIR_NAME" \
     "ParameterKey=ServerCertificate,ParameterValue=$(cat data/server.crt)" \
     "ParameterKey=CACertificate,ParameterValue=$(cat data/ca.crt)"
 
 # Wait for the action to be complete.
-aws cloudformation wait stack-create-complete --stack-name openvpn
+aws cloudformation wait ${EVENT} --stack-name openvpn
 
-echo "create-stack action is complete"
+echo "$ACTION action is complete"
