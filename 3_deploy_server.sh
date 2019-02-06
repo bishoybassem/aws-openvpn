fb362b9455899de894f0e02c9af0f0acbcb56450
bishoybassem <bishoybassem@gmail.com>
2019-02-06 16:22:12 +0100

    Pass the server's private key as a parameter with no echo

--- 3_deploy_server.sh
+++ 3_deploy_server.sh
@@ -10,7 +10,7 @@ fi
 ACTION=create-stack
 EVENT=stack-create-complete
 # If the stack already exists, then use the update action.
-if [[ $(aws cloudformation describe-stacks --stack-name openvpn 2> /dev/null) ]]; then
+if aws cloudformation describe-stacks --stack-name openvpn &> /dev/null; then
     ACTION=update-stack
     EVENT=stack-update-complete
 fi
@@ -19,7 +19,8 @@ fi
 aws cloudformation ${ACTION} --stack-name openvpn --template-body file://cf_template.yml --parameters \
     "ParameterKey=KeyName,ParameterValue=$KEY_PAIR_NAME" \
     "ParameterKey=ServerCertificate,ParameterValue=$(cat data/server.crt)" \
-    "ParameterKey=CACertificate,ParameterValue=$(cat data/ca.crt)"
+    "ParameterKey=CACertificate,ParameterValue=$(cat data/ca.crt)" \
+    "ParameterKey=ServerPrivateKey,ParameterValue=$(cat data/server.pem)"
 
 # Wait for the action to be complete.
 aws cloudformation wait ${EVENT} --stack-name openvpn
